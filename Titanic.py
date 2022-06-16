0379ed53eec2b002926f4fb90e1118d56b71e71e
Cristian Gofiar <cristiangofiar@gmail.com>
2022-06-16 01:11:48 -0300

    Data preparation

--- Titanic.py
+++ Titanic.py
@@ -1,3 +1,20 @@
-import tensorflow as tf
+#import tensorflow as tf
 import numpy as np
+import pandas as pd
+
+from sklearn.preprocessing import LabelEncoder
+from sklearn.impute import SimpleImputer
+
+
+train_data = pd.read_csv('data/train.csv')
+test_data = pd.read_csv('data/test.csv')
+
+y_train = train_data.Survived
+X_train = train_data.drop(['Name','SibSp','Parch','Ticket','Fare','Cabin','Embarked','Survived'], axis=1)
+
+#  Reemplazar datos categóricos por 0s y 1s.
+X_train['Sex'] = LabelEncoder().fit_transform(X_train['Sex'])
+
+# Imputar valores vacios con el valor medio de la edad
+X_train['Age'] = X_train['Age'].fillna(X_train['Age'].mean())
 
